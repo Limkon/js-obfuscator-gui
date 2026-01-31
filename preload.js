0b7092ed1af0e094add268151675a1a45e355b76
Limkon <kconlim+1@gmail.com>
2026-01-31 23:00:10 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     selectFile: () => ipcRenderer.invoke('dialog:openFile'),
-    obfuscate: (data) => ipcRenderer.invoke('perform-obfuscate', data)
+    obfuscate: (data) => ipcRenderer.invoke('perform-obfuscate', data),
+    showContextMenu: () => ipcRenderer.send('show-context-menu')
 });
